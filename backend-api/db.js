c66888806d5b16f369285812790f0dec6b12ea73
Yaslly-dev <yasespigolani@gmail.com>
2025-11-26 19:54:26 -0300

    Removendo senha exposta no db.js

--- backend-api/db.js
+++ backend-api/db.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 // LEMBRA DE USAR O SEU AI, VICTOR ESTA USANDO A DELE AQUI
 
 const pool = mysql.createPool({
-  host: 'localhost',       // seu MySQL local
-  user: 'root',            // usuário do MySQL
-  password: 'Yg76211234!',      // senha do MySQL 
-  database: 'future_ai',   // nome do banco 
+  host: "localhost", // seu MySQL local
+  user: "root", // usuário do MySQL
+  password: "", // senha do MySQL
+  database: "future_ai", // nome do banco
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
